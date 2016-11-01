28e7db2fc67e6a2a128ca088dc12b407adf30c02
maravilla <maravilla@users.noreply.github.com>
2016-10-31 19:50:28 -0600

    Add files via upload

---
+++ Nutriologa/paciente.cpp
@@ -0,0 +1,171 @@
+#include "paciente.h"
+
+#include <string>
+
+Paciente::Paciente(){
+
+}
+
+Paciente::Paciente(const Paciente&){
+
+}
+
+Name Paciente::getName(){
+
+}
+
+Date Paciente::getBirthDay(){
+
+}
+
+int Paciente::getAge(){
+
+}
+
+char Paciente::getSex(){
+
+}
+
+std::string Paciente::getAddress(){
+
+}
+
+int Paciente::getPhone(){
+
+}
+
+int Paciente::getCellphone(){
+
+}
+
+std::string Paciente::getOccupation(){
+
+}
+
+std::string Paciente::getMartialstatus(){
+
+}
+
+bool Paciente::getPlaysSports(){
+
+}
+
+bool Paciente::getSmokes(){
+
+}
+
+bool Paciente::getDrinks(){
+
+}
+
+float Paciente::getWeight(){
+
+}
+
+float Paciente::getHeight(){
+
+}
+
+Date Paciente::getstartingDate(){
+
+}
+
+Date Paciente::getAppointment(){
+
+}
+
+void Paciente::setName(const Name&){
+
+}
+
+void Paciente::setBirthDay(const Date&){
+
+}
+
+void Paciente::setstartingDate(const Date&){
+
+}
+
+void Paciente::setAge(const int&){
+
+}
+
+void Paciente::setSex(const char&){
+
+}
+
+void Paciente::setAddress(const std::string){
+
+}
+
+void Paciente::setPhone(const int&){
+
+}
+
+void Paciente::setCellphone(const int&){
+
+}
+
+void Paciente::setOccupation(const std::string){
+
+}
+
+void Paciente::setMartialstatus(const std::string){
+
+}
+
+void Paciente::setPlaysSports(){
+
+}
+
+void Paciente::setSmokes(){
+
+}
+
+void Paciente::setDrinks(){
+
+}
+
+void Paciente::setWeight(const float&){
+
+}
+
+void Paciente::setHeight(const float&){
+
+}
+
+void Paciente::setStartingDate(){
+
+}
+
+void Paciente::setAppointment(){
+
+}
+
+float Paciente::calculateBMI(const float&, const float&){
+
+}
+
+bool Paciente::operator==(const Paciente&){
+
+}
+
+bool Paciente::operator!=(const Paciente&){
+
+}
+
+bool Paciente::operator<(const Paciente&){
+
+}
+
+bool Paciente::operator>(const Paciente&){
+
+}
+
+bool Paciente::operator<=(const Paciente&){
+
+}
+
+bool Paciente::operator>=(const Paciente&){
+
+}
---
+++ Nutriologa/paciente.h
@@ -0,0 +1,86 @@
+#ifndef PACIENTE_H_INCLUDED
+#define PACIENTE_H_INCLUDED
+
+
+#include <string>
+#include "name.h"
+#include "date.h"
+
+class Paciente{
+private:
+   
+    Name name;
+    Date birthDay;
+    int age;
+    char sex;
+    std::string address;
+    int phone;
+    int cellphone;
+    std::string occupation;
+    std::string martialStatus;
+    bool playsSports;
+    bool smokes;
+    bool drinks;
+    float weight;
+    float height;
+    Date startingDate;
+    Date appointment;
+
+public:
+
+    Paciente();
+    Paciente(const Paciente&);
+
+    Name getName();
+    Date getBirthDay();
+    int getAge();
+    char getSex();
+    std::string getAddress();
+    int getPhone();
+    int getCellphone();
+    std::string getOccupation();
+    std::string getMartialstatus();
+    bool getPlaysSports();
+    bool getSmokes();
+    bool getDrinks();
+    float getWeight();
+    float getHeight();
+    Date getstartingDate();
+    Date getAppointment();
+
+    void setName(const Name&);
+    void setBirthDay(const Date&);
+    void setstartingDate(const Date&);
+    void setAge(const int&);
+    void setSex(const char&);
+    void setAddress(const std::string);
+    void setPhone(const int&);
+    void setCellphone(const int&);
+    void setOccupation(const std::string);
+    void setMartialstatus(const std::string);
+    void setPlaysSports();
+    void setSmokes();
+    void setDrinks();
+    void setWeight(const float&);
+    void setHeight(const float&);
+    void setStartingDate();
+    void setAppointment();
+    float calculateBMI(const float&, const float&);
+
+
+    bool operator == (const Paciente&);
+    bool operator != (const Paciente&);
+    bool operator < (const Paciente&);
+    bool operator > (const Paciente&);
+    bool operator <= (const Paciente&);
+    bool operator >= (const Paciente&);
+
+
+
+    friend std::ostream& operator << (std::ostream& , Paciente&);
+    friend std::istream& operator >> (std::istream&, Paciente&);
+
+
+};
+
+#endif // PACIENTE_H_INCLUDED
